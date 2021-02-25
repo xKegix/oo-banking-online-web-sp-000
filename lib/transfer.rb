479e22589b656c49eb4f708d7d110045d4b76d2c
Keginold Philip <keginoldp@hotmail.com>
2021-02-25 16:21:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,7 +11,7 @@ class Transfer
   end
 
 def valid?
-  @satus == 'pending' && @sender.valid? && @sender.balance > @amount 
+  @status == 'pending' && @sender.valid? && @sender.balance > @amount 
 end
 
 
