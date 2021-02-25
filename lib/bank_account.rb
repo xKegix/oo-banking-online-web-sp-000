cb827d02b52bc6feab6da2a34d46d0bf5bf5db04
Keginold Philip <keginoldp@hotmail.com>
2021-02-25 15:57:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,9 +6,12 @@ attr_reader :name
   def initialize(name)
     @name = name
     @balance = 1000
-    @status = "open" 
+    @status = "open"
   end
 
+def deposit(money)
+  @balance += money
+end
 
 
 
